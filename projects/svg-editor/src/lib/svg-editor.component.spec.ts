8ad6ee7c46f8da05bca34964264e3ee675cab180
Rajesh <rajesh.kumaravel@tarento.com>
2021-12-16 12:01:12 +0530

    code clean up

--- projects/svg-editor/src/lib/svg-editor.component.spec.ts
+++ projects/svg-editor/src/lib/svg-editor.component.spec.ts
@@ -79,3 +79,4 @@ describe('SvgEditorComponent', () => {
 
 });
 
+
--- projects/svg-editor/src/lib/svg-editor.component.ts
+++ projects/svg-editor/src/lib/svg-editor.component.ts
@@ -10,7 +10,7 @@ import * as _ from 'lodash-es';
 })
 
 export class SvgEditorComponent implements OnInit {
-  [x: string]: any;
+  showPreviewButton: boolean = false;
 
   ICON_STRING: string = 'M 18.414062 2 C 18.158062 2 17.902031 2.0979687 17.707031 2.2929688 L 5 15 C 5 15 6.005 15.005 6.5 15.5 C 6.995 15.995 6.984375 16.984375 6.984375 16.984375 C 6.984375 16.984375 8.003 17.003 8.5 17.5 C 8.997 17.997 9 19 9 19 L 21.707031 6.2929688 C 22.098031 5.9019687 22.098031 5.2689063 21.707031 4.8789062 L 19.121094 2.2929688 C 18.926094 2.0979688 18.670063 2 18.414062 2 z M 18.414062 4.4140625 L 19.585938 5.5859375 L 18.537109 6.6347656 L 17.365234 5.4628906 L 18.414062 4.4140625 z M 15.951172 6.8769531 L 17.123047 8.0488281 L 9.4609375 15.710938 C 9.2099375 15.538938 8.9455469 15.408594 8.6855469 15.308594 C 8.5875469 15.050594 8.4590625 14.789063 8.2890625 14.539062 L 15.951172 6.8769531 z M 3.6699219 17 L 3 21 L 7 20.330078 L 3.6699219 17 z';
   @Input() svgContent!: String;
@@ -38,9 +38,6 @@ export class SvgEditorComponent implements OnInit {
       // Determine the coordinates of the text tag
       let bBox = (svgElement as unknown as SVGSVGElement).getBBox();
 
-      // console.log('ele ', _elementId); // TODO: log!
-      // console.table(bBox)
-
       // Create pen icon wrap for text
       let svgTag = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
       svgTag.setAttribute('x', (bBox['x'] + bBox['width']).toString());
@@ -59,17 +56,14 @@ export class SvgEditorComponent implements OnInit {
       let editIconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
       editIconPath.setAttribute('id', 'pathIdD');
       editIconPath.setAttribute('d', this.ICON_STRING);
-      // editIconPath.setAttribute('stroke', 'black');
-      // editIconPath.setAttribute('stroke-width', 3);
       editIconPath.setAttribute('opacity', '1');
       editIconPath.setAttribute('fill', 'red');
 
       svgTag.append(editIconPath);
-      console.log('svgElement ', svgElement); // TODO: log!
       // Add event listener for text
       svgElement.addEventListener('click', (e) => {
         // Modal popup for text input
-        this.svgElementClicked(e, svgElement, 'text');
+        this.svgElementClicked(svgElement, 'text');
       });
 
       // Add pen icon to SVG DOM to end of text tag
@@ -107,8 +101,6 @@ export class SvgEditorComponent implements OnInit {
       let editIconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
       editIconPath.setAttribute('id', 'pathIdD');
       editIconPath.setAttribute('d', this.ICON_STRING);
-      // editIconPath.setAttribute('stroke', 'black');
-      // editIconPath.setAttribute('stroke-width', 3);
       editIconPath.setAttribute('opacity', '1');
       editIconPath.setAttribute('fill', 'red');
 
@@ -116,7 +108,7 @@ export class SvgEditorComponent implements OnInit {
       // Add event listener for image
       imageElement.addEventListener('click', (e) => {
         // Modal popup for image input
-        this.svgElementClicked(e, imageElement, 'image');
+        this.svgElementClicked(imageElement, 'image');
       });
 
       // Add pen icon to SVG DOM to end of image tag
@@ -141,8 +133,19 @@ export class SvgEditorComponent implements OnInit {
     // this.showSVGInputModal = true;
   }
 
-  svgElementClicked(ele: any, svgElement: any, type: any) {
-    this.elementClicked.emit();
+  svgElementClicked(svgElement: any, type: any) {
+    // if (!this.showPreviewButton) return;
+    var selectedSVGObject = {
+      type: type,
+      value: svgElement.textContent,
+      element: svgElement,
+      svgElement: svgElement
+    };
+    console.table(selectedSVGObject); // TODO: log!
+    this.elementClicked.emit({
+      type: type,
+      element: svgElement
+    });
   }
 
   setElementId (e: any, index: any, type: any) {
