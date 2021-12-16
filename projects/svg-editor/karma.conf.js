bfbf819735107fb1f018ad729c0bb6012794a3b5
Rajesh <rajesh.kumaravel@tarento.com>
2021-12-16 17:28:19 +0530

    Updated karma config

--- projects/svg-editor/karma.conf.js
+++ projects/svg-editor/karma.conf.js
@@ -12,18 +12,17 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    browserNoActivityTimeout: 100000,
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        timeoutInterval: 300000
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
+    // coverageIstanbulReporter: {
+    //   dir: require('path').join(__dirname, './coverage'), reports: [ 'html', 'lcovonly' ],
+    //   fixWebpackSourcePaths: true
+    // },
     coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/svg-editor'),
       subdir: '.',
@@ -38,7 +37,22 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+          '--proxy-server=direct://',
+          '--proxy-bypass-list=*',
+          '--max_old_space_size=4096'
+        ]
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
