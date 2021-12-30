d892aa06ab80ff941b1b9c5b401b26e3354b21b2
Rajesh <rajesh.kumaravel@tarento.com>
2021-12-30 15:39:03 +0530

    ng changes added

--- projects/svg-editor/src/lib/svg-editor.component.ts
+++ projects/svg-editor/src/lib/svg-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 import * as _ from 'lodash-es';
@@ -10,7 +10,7 @@ import { Subject } from 'rxjs/internal/Subject';
   styles: []
 })
 
-export class SvgEditorComponent implements OnInit, OnDestroy {
+export class SvgEditorComponent implements OnInit, OnDestroy, OnChanges {
   showPreviewButton: boolean = false;
 
   @Input() icon: string = 'M 18.414062 2 C 18.158062 2 17.902031 2.0979687 17.707031 2.2929688 L 5 15 C 5 15 6.005 15.005 6.5 15.5 C 6.995 15.995 6.984375 16.984375 6.984375 16.984375 C 6.984375 16.984375 8.003 17.003 8.5 17.5 C 8.997 17.997 9 19 9 19 L 21.707031 6.2929688 C 22.098031 5.9019687 22.098031 5.2689063 21.707031 4.8789062 L 19.121094 2.2929688 C 18.926094 2.0979688 18.670063 2 18.414062 2 z M 18.414062 4.4140625 L 19.585938 5.5859375 L 18.537109 6.6347656 L 17.365234 5.4628906 L 18.414062 4.4140625 z M 15.951172 6.8769531 L 17.123047 8.0488281 L 9.4609375 15.710938 C 9.2099375 15.538938 8.9455469 15.408594 8.6855469 15.308594 C 8.5875469 15.050594 8.4590625 14.789063 8.2890625 14.539062 L 15.951172 6.8769531 z M 3.6699219 17 L 3 21 L 7 20.330078 L 3.6699219 17 z';
@@ -37,6 +37,12 @@ export class SvgEditorComponent implements OnInit, OnDestroy {
         this.toggleSVGPreview(data);
       });
     }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    setTimeout(() => {
+      this.generateMaskForSvgElements();
+    }, 1000);
   }
 
   ngOnDestroy() {
